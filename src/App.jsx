9f00f6bc793e6fbcd6a6fa5677a76b77bd93c722
BrijStack <brijesh17091999@gmail.com>
2024-09-03 11:01:20 +0530

    3-09-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./component/CityList";
+import CountryList from "./component/CountryList";
+import City from "./component/City";
+import Form from "./component/Form";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 };
 
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,14 @@
+import Map from "../component/Map";
+import Sidebar from "../component/Sidebar";
+import styles from "./AppLayout.module.css";
+
+const AppLayout = () => {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
+};
+
+export default AppLayout;
